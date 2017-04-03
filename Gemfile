2b4cb0f8c0f7ad4f0ddbd74144a1b500af5f8efc
Simião Carvalho <simiaosimis@gmail.com>
2017-04-03 19:10:31 -0300

    Add devise and materialize gems

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ git_source(:github) do |repo_name|
 end
 
 gem 'devise'
+gem 'materialize-sass'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.2'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+	devise_for :users, path: '/', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'new' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
