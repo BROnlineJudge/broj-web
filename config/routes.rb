8d3c8a657eea97f3018f9a8083c4ae3b10df878f
Simião Carvalho <simiaosimis@gmail.com>
2017-04-04 19:29:53 -0300

    Add navbar and HomePage

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :problems
 	devise_for :users, path: '/', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'new' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home_page#index'
 end
