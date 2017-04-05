aa3df4895dea2c69baf8718f5450b29bf2350971
CaioIcy <caio.nardelli@hotmail.com>
2017-04-04 21:31:44 -0300

    bootstrap 4
    Signed-off-by: CaioIcy <caio.nardelli@hotmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users, path: '/', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    password: 'secret',
+    confirmation: 'verification',
+    unlock: 'unblock',
+    registration: 'register',
+    sign_up: 'new'
+  }
+
   resources :problems
-	devise_for :users, path: '/', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'new' }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root to: 'home_page#index'
 end
