d79c8388ac9f1271aad75e36a2e9233201582ff5
Simião Carvalho <simiaosimis@gmail.com>
2017-04-12 23:34:57 -0300

    Add admin role

--- db/migrate/20170403204202_devise_create_users.rb
+++ db/migrate/20170403204202_devise_create_users.rb
@@ -4,6 +4,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :name,               null: false, default: ""
+      t.boolean :admin,             default: false
 
       ## Recoverable
       t.string   :reset_password_token
